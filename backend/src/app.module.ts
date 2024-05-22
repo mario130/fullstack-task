47c8d4feb9ae5eeba7ec246df22eab16ec7203ce
Mario Yonan <62141485+mario130@users.noreply.github.com>
2024-05-22 19:34:04 +0300

    init mongodb

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { mongoConfig } from './config/mongo.config';
 
 @Module({
-  imports: [],
+  imports: [mongoConfig],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/config/mongo.config.ts
@@ -0,0 +1,5 @@
+import { MongooseModule } from '@nestjs/mongoose';
+
+export const mongoConfig = MongooseModule.forRoot(
+  'mongodb://localhost:27017/fullstacktest',
+);
