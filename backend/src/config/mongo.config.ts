6fc17e23d3dbd3fde1b1236983deae52229a77e3
Mario Yonan <62141485+mario130@users.noreply.github.com>
2024-05-22 19:54:46 +0300

    feat: add user module and schema

--- backend/src/config/mongo.config.ts
+++ backend/src/config/mongo.config.ts
@@ -1,5 +1,5 @@
 import { MongooseModule } from '@nestjs/mongoose';
 
 export const mongoConfig = MongooseModule.forRoot(
-  'mongodb://localhost:27017/fullstacktest',
+  'mongodb://localhost:27017/easygenerator-backend',
 );
