a5308936d5af3f43c325ba4470125147dce3c0c5
Mario Yonan <62141485+mario130@users.noreply.github.com>
2024-05-22 20:57:51 +0300

    feat: add user dto, signup for user controller

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/creat-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('signup')
+  async signUp(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(
+      createUserDto.email,
+      createUserDto.name,
+      createUserDto.password,
+    );
+  }
+}
