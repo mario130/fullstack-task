165a076c4c21f4e177ce3d82f5a6a180250e4742
Mario Yonan <62141485+mario130@users.noreply.github.com>
2024-05-22 19:59:39 +0300

    feat: update users service for creating/getting users

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async createUser(
+    email: string,
+    name: string,
+    password: string,
+  ): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new this.userModel({
+      email,
+      name,
+      password: hashedPassword,
+    });
+    return newUser.save();
+  }
+
+  async findUserByEmail(email: string): Promise<User> {
+    return this.userModel.findOne({ email }).exec();
+  }
+}
