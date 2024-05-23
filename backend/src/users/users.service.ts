690b13c5c494c6c176e1c0340d656b5addfdb293
Mario Yonan <62141485+mario130@users.noreply.github.com>
2024-05-23 21:18:26 +0300

    feat: throw error if email is not unique

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
@@ -13,6 +13,11 @@ export class UsersService {
     name: string,
     password: string,
   ): Promise<User> {
+    const existingUser = await this.findUserByEmail(email);
+    if (existingUser) {
+      throw new ConflictException('Email already exists');
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new this.userModel({
       email,
