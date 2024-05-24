21be0a292919cde65618fe7be3706c1ac69c42f4
Mario Yonan <62141485+mario130@users.noreply.github.com>
2024-05-24 15:50:19 +0300

    refactor: user service

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  BadRequestException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
@@ -13,6 +17,15 @@ export class UsersService {
     name: string,
     password: string,
   ): Promise<User> {
+    email = email.trim().toLowerCase();
+    name = name.trim();
+
+    if (!this.isValidPassword(password)) {
+      throw new BadRequestException(
+        'Password does not meet complexity requirements',
+      );
+    }
+
     const existingUser = await this.findUserByEmail(email);
     if (existingUser) {
       throw new ConflictException('Email already exists');
@@ -29,5 +42,13 @@ export class UsersService {
 
   async findUserByEmail(email: string): Promise<User> {
     return this.userModel.findOne({ email }).exec();
+  }
+
+  private isValidPassword(password: string): boolean {
+    const hasLetter = /[A-Za-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSpecialChar = /[@$!%*?&]/.test(password);
+    const isValidLength = password.length >= 8;
+    return hasLetter && hasNumber && hasSpecialChar && isValidLength;
   }
 }
